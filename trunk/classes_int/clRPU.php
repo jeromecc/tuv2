d61267f29fdcf8da209b5b342f15b70c6362d607
emmanuel.cervetti <emmanuel.cervetti@df6dc446-1535-11de-b091-d144e675ee08>
2009-07-17 14:00:01 +0000

    plus de limitation de temps sur l'exécution

--- trunk/classes_int/clRPU.php
+++ trunk/classes_int/clRPU.php
@@ -15,6 +15,10 @@ class clRPU {
 
   	// Constructeur.
   	function __construct ( $param='' ) {
+
+		set_time_limit(0);
+		ini_set('memory_limit', '512M');
+
  		if ( $param == 'showLogs' ) $this->showLogs ( ) ;
  		else $this->gestSend ( ) ;
   	}
