ce6b7d49459fba2cba5acce631d8484af656307d
titeiko <titeiko@df6dc446-1535-11de-b091-d144e675ee08>
2009-11-06 13:08:15 +0000

    Formulaire bio test pour fréjus + correction bug formulaire AEV

--- formx/functions/Test_AEV_Situation_23.php
+++ formx/functions/Test_AEV_Situation_23.php
@@ -2,12 +2,15 @@
 function Test_AEV_Situation_23($formx) {
 
 // On determine dans quelle situation on se trouve
-$AEV_Type = utf8_decode($formx->getFormVar('AEV_Situation'));
+    $AEV_Type = utf8_decode($formx->getFormVar('AEV_Situation'));
 
 
-if ( (int)$AEV_Type == 23 )
-  {eko("1");return true;}
-else
-  {eko("0");return false;}
+    //if ( (int)$AEV_Type == 23 )
+    //  {eko("1");return true;}
+    //else
+    //  {eko("0");return false;}
+    //}
+
+    return ((int)$AEV_Type == 23);
 }
 ?>
